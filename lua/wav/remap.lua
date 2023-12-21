ece5d0e23530c13d5cae325a1343a927d30fafc6
Taj Parmanand-Wilson <Taj.Parmanand-Wilson@kbr.com>
2023-12-21 19:35:18 +1100

    Added keymap settings

--- lua/wav/remap.lua
+++ lua/wav/remap.lua
@@ -14,5 +14,23 @@ end)
 vim.opt.foldmethod = 'indent'
 vim.opt.foldenable = false
 vim.keymap.set('n', '<leader>f', function()
-	vim.opt.foldenable = not vim.o.foldenable
+    vim.opt.foldenable = not vim.o.foldenable
 end)
+
+-- Movement
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('v', 'p', '"_dP')
+vim.keymap.set('n', 'd', '"_d')
+vim.keymap.set('v', 'd', '"_d')
+vim.keymap.set('v', '<C-c>', '"*y')
+vim.keymap.set('v', '<C-v>', '"*p')
+vim.keymap.set('n', '<C-c>', '"*y')
+vim.keymap.set('n', '<C-v>', '"*p')
+vim.keymap.set('n', 'Q', '<nop>')
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
