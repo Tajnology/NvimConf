a73e9227e682fcad323f43ccbaeaacc69fc0fa40
Taj Parmanand-Wilson <Taj.Parmanand-Wilson@kbr.com>
2023-12-21 14:20:33 +1100

    Revert "Update harpoon.lua"
    This reverts commit b51a212dee4db56337788de11565927ba55fca1e.

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -21,7 +21,6 @@ harpoon:setup(harpoon_config)
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 vim.keymap.set("n", "<leader>w", function() harpoon:list():remove() end)
-vim.keymap.set("n", "<leader><C-w>", function() harpoon:list():clear() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
