8ea2ec101f0c2620db53b774450b92854126ca6d
Taj Parmanand-Wilson <Taj.Parmanand-Wilson@kbr.com>
2023-12-21 15:07:50 +1100

    Moved treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,18 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
- }
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function ()
+        local configs = require('nvim-treesitter.configs')
+
+        configs.setup({
+            ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'xml', 'javascript', 'html' },
+            sync_install = false,
+            auto_install = true,
+            highlight = { 
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = false },
+        })
+    end
+}
