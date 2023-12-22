2e508c8f32683b307bd62570a197f657f2f75367
tajnology <tajnology@gmail.com>
2023-12-22 17:00:08 +1100

    Minor improvements

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,11 @@
 return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	dependencies = { 'nvim-lua/plenary.nvim' }
+	dependencies = {
+        'nvim-lua/plenary.nvim',
+        'akinsho/nvim-toggleterm.lua',
+        'sopa0/telescope-makefile',
+    },
+    config = function()
+        require('telescope').load_extension('make')
+    end,
 }
